8c890d061eb37a69eb0949c4531cbf20fb119a96
Alvin-Tulud <alvintulud@gmail.com>
2024-12-10 19:21:22 -0800

    start stun state that will be called by stuff later

--- Armored Fisticuffs/Assets/Scripts/Character/StateMachine/Stun_State.cs
+++ Armored Fisticuffs/Assets/Scripts/Character/StateMachine/Stun_State.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Stun_State : State
 {
+    [SerializeField]
+    private LayerMask ignorePlayer;
+
     private Aerial_State Aerial_;
     private Grounded_State Grounded_;
+
+    private bool inStun;
+
 
     void Start()
     {
         Aerial_ = GetComponent<Aerial_State>();
         Grounded_ = GetComponent<Grounded_State>();
+
+        inStun = false;
     }
 
     public override State ChangeState()
@@ -18,12 +26,20 @@ public class Stun_State : State
 
     public override State RunCurrentState()
     {
-        throw new System.NotImplementedException();
+
+
+        return this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * 1f), Color.red, 1);
+        return Physics2D.Raycast(transform.position, Vector3.down, 1f, ignorePlayer);
     }
 }
