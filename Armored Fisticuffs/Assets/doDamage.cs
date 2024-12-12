f7448025e16eb164a1f269887fda82a05c50a92e
Alvin-Tulud <alvintulud@gmail.com>
2024-12-11 17:23:49 -0800

    fix anims moving dmg box

---
+++ Armored Fisticuffs/Assets/doDamage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class doDamage : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
